4d35b63cb368a30b5fe405a4a4d9a28eb8a9b8bc
Yoshimura Hiroki <hiroshuke0624@gmail.com>
2021-06-02 17:04:48 +0900

    viewer完成

--- src/components/ExamplePageViewer.js
+++ src/components/ExamplePageViewer.js
@@ -1,10 +1,22 @@
 import React, { Component ,  useState } from 'react';
 import CornerstoneViewport from 'react-cornerstone-viewport';
-import cornerstoneTools from 'cornerstone-tools'
-
+import cornerstoneTools from 'cornerstone-tools';
 import { head_CT_dicom ,anno_head } from "./Head";
 import { thorax_dicom  } from "./Thorax";
 import { abdomen_dicom } from "./Abdomen";
+import Angleimg from "./images/Icon/Angle.png";
+import ArrowAnnotateimg from "./images/Icon/ArrowAnnotate.png";
+import Bidirectionalimg from "./images/Icon/Bidirectional.png";
+import Circleimg from "./images/Icon/Circle.png";
+import Dragimg from "./images/Icon/Drag.png";
+import Eraserimg from "./images/Icon/Eraser.png";
+import Freehandimg from "./images/Icon/Freehand.png";
+import Lengthimg from "./images/Icon/Length.png";
+import Rectangleimg from "./images/Icon/Rectangle.png";
+import Resetimg from "./images/Icon/Reset.png";
+import Rotateimg from "./images/Icon/Rotate.png";
+import Wwwcimg from "./images/Icon/Wwwc.png";
+import Zoomimg from "./images/Icon/Zoom.png";
 
 const stack1 = [
     'dicomweb://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.7.dcm',
@@ -40,13 +52,29 @@ const img_list_name = {
     'thorax':['胸部CT'],
     'abdomen':['腹部CT'],
 }
-
-var stylelist = ['viewer','viewer','viewer','viewer','viewer']
-
-const reset_stylelist = function(stylelist){
+function mklist(name,num) {
     var new_list = []
-    for(let i=0;i<=stylelist.length-1;i++){
-        new_list.push('viewer')
+    for(let i=0;i<=num-1;i++){
+        new_list.push(name)
+    }
+    return new_list
+};
+
+var stylelist = mklist('viewer',5)
+var toolstylelist = mklist('tool_box',12)
+const toolimglist = [Angleimg,ArrowAnnotateimg,Bidirectionalimg,Circleimg,Dragimg,
+    Eraserimg,Freehandimg,Lengthimg,Rectangleimg,Rotateimg,Wwwcimg,Zoomimg];
+const toolnamelist = ["Angle",'ArrowAnnotate',"Bidirectional","CircleRoi","Pan",
+"Eraser","FreehandRoi","Length","RectangleRoi","Rotate","Wwwc","Zoom"];
+const toollabel = ["角度測定",'矢印',"十字測定","円描写","ドラッグ",
+"消しゴム","手書き","長さ測定","四角描写","回転","　　　","ズーム"];
+
+
+
+const reset_list = function(oldlist,name){
+    var new_list = []
+    for(let i=0;i<=oldlist.length-1;i++){
+        new_list.push(name)
     }
     return new_list
 };
@@ -383,6 +411,7 @@ class Viewer extends Component {
     state = {
         activeViewportIndex: 0,
         viewports: [0,1,2,3,4],
+        toolports: [0,1,2,3,4,5,6,7,8,9,10,11],
         tools: [
             // Mouse
             {
@@ -407,158 +436,112 @@ class Viewer extends Component {
             {name:'FreehandRoi',mode: 'active' },
             {name:'Eraser', mode: 'active' },
             {name:'ArrowAnnotate',mode:'active'},
+            {name: 'CircleRoi',mode:'active'},
+            {name:'RectangleRoi',mode:'active'},
             // Scroll
             { name: 'StackScrollMouseWheel', mode: 'active' },
             // Touch
             { name: 'PanMultiTouch', mode: 'active' },
             { name: 'ZoomTouchPinch', mode: 'active' },
             { name: 'StackScrollMultiTouch', mode: 'active' },
-            {name: 'CircleRoi',mode:'active'},
-            {name:'RectangleRoi',mode:'active'}
+            {name: 'Rotate',mode:'active'},
         ],
         imageIds: dicomdata[this.props.myprop],
         quiz: quiz_dict[this.props.myprop],
         // FORM
         activeTool: 'Wwwc',
+        activeToolIndex: 10,
         imageIdIndex: 0,
         isPlaying: false,
         frameRate: 5,
         showProps: true,
         style: 'viewers',
         viewerstyle: stylelist,
+        toolstyle: toolstylelist,
     };
 
     componentDidMount() { }
 
     render() {
-        //var element = document.getElementById(imageIds);
-        //cornerstoneTools.length.enable(element);
-        //var Toolstate = cornerstoneTools.getToolState({element:element, toolType:'CircleScissors'})
-        var toolState = cornerstoneTools.globalImageIdSpecificToolStateManager.saveToolState();
         return (
         <div className='practice_contents'>
             <div >
-                <p>{console.log(toolState)}</p>
-                <p>{console.log(toolState.name)}</p>
-                <p>{console.log(image)}</p>
                 <div className='viewer_propaties'>
-                        <form className="viewer_row">
-                            {/* FIRST COLUMN */}
-                            <div className = 'tool_contents'>
-                                <div
-                                className="tool_box"
-                                type="button"
+                    <div className='propaty'>
+                    <label htmlFor="active-tool">Tool Contents</label>
+                        <div className = 'tool_contents'>
+                            {this.state.toolports.map((index) => (
+                            <div className={this.state.toolstyle[index]}
                                 onClick={() => {
-                                    this.setState({activeTool: 'CircleRoi'});
-                                     }}>
-                                    〇
-                                </div>
-                                <button
-                                className="tool_btn"
-                                type="button"
-                                onClick={() => {
-                                    this.setState({activeTool: 'Eraser'});
-                                     }}>
-                                    消しゴム
-                                </button>
+                                    toolstylelist=reset_list(toolstylelist,'tool_box')
+                                    toolstylelist[index]='tool_box_click';
+                                    this.setState({activeTool: toolnamelist[index],
+                                        toolstyle: toolstylelist,
+                                        activeToolIndex: index,
+                                            });
+                                    }}>
+                                <img className="tool_icon" src={toolimglist[index]} title={toollabel[index]}></img>
                             </div>
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label htmlFor="active-tool">Active Tool:</label>
-                                    <select
-                                    value={this.state.activeTool}
-                                    onChange={evt =>
-                                        this.setState({ activeTool: evt.target.value })
-                                    }
-                                    className="form-control"
+                            ))}
+                        </div>
+                    </div>
+                    <div className='propaty'>
+                    <label htmlFor="active-tool">Viwer Size</label>
+                        <div  className='window'
+                            onClick={() => {
+                            stylelist=reset_list(stylelist,'viewer')
+                            this.setState({
+                            style: 'viewers',
+                            viewerstyle: stylelist
+                               });
+                               }}>
+                            <img className="tool_img" src={Resetimg} alt="画像配置をリセット" title="画像配置をリセット"></img>
+                        </div>
+                    </div>
+                    <div className="propaty">
+                    <label htmlFor="active-tool">Tool Status</label>
+                        <div className="tool_statuses">
+                            <div className="tool_status">
+                                <label htmlFor="active-tool">Active Tool:</label>
+                                <input
+                                    type = "text"
+                                    value={toollabel[this.state.activeToolIndex]}
+                                    readOnly={true}
+                                    className="form_tool"
                                     id="active-tool"
                                     >
-                                    <option value="Wwwc">Wwwc</option>
-                                    <option value="Zoom">Zoom</option>
-                                    <option value="Pan">Pan</option>
-                                    <option value="Length">Length</option>
-                                    <option value="Angle">Angle</option>
-                                    <option value="Bidirectional">Bidirectional</option>
-                                    <option value="FreehandRoi">Freehand</option>
-                                    <option value='ArrowAnnotate'>ArrowAnnotate</option>
-                                    <option value="Eraser">Eraser</option>
-                                    <option value="CircleRoi">CircleScissors</option>
-                                    <option value="RectangleRoi">RectangleRoi</option>
-                                    </select>
-                               </div>
-                                <div className="form-group">
-                                    <label htmlFor="image-id-index">Image ID Index:</label>
-                                    <input
-                                    type="range"
-                                    min="0"
-                                    max={this.state.imageIds.length - 1}
-                                    value={this.state.imageIdIndex}
-                                    onChange={evt =>
-                                        this.setState({ imageIdIndex: parseInt(evt.target.value) })
-                                    }
-                                    className="form-control"
-                                    id="image-id-index"
-                                    ></input>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="image-id-stack">Image ID Stack:</label>
-                                    <select
-                                    defaultValue={1}
-                                    onChange={evt => {
-                                        const selectedStack =
-                                            parseInt(evt.target.value) === 1 ? stack1 : stack2;
-
-                                        this.setState({
-                                            imageIds: selectedStack,
-                                            imageIdIndex: 0,
-                                        });
-                                    }}
-                                    className="form-control"
-                                    id="image-id-stack"
-                                    >
-                                    <option value="1">Stack 1</option>
-                                    <option value="2">Stack 2</option>
-                                    </select>
+                                </input>
+                            </div>
+                            <div className="tool_status">
+                                <label htmlFor="active-viewport-index">
+                                Active Viewport Index:
+                                </label>
+                                <input
+                                type="text"
+                                readOnly={true}
+                                value={this.state.activeViewportIndex}
+                                className="form_tool"
+                                id="active-viewport-index"
+                                ></input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='propaty'>
+                        <label htmlFor="active-tool">Information</label>
+                            <div className='complain'>
+                                <div>
+                                    <h3>63歳　男性</h3>
+                                    <p>精査希望</p>
                                 </div>
                             </div>
-                            {/* SECOND COLUMN */}
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label htmlFor="active-viewport-index">
-                                    Active Viewport Index:
-                                    </label>
-                                    <input
-                                    type="text"
-                                    readOnly={true}
-                                    value={this.state.activeViewportIndex}
-                                    className="form-control"
-                                    id="active-viewport-index"
-                                    ></input>
-                                </div>
-                            </div>
-                        </form>
-                </div>
-                <div>
-                    <button
-                    className="body_btn"
-                    type="button"
-                    onClick={() => {
-                        stylelist=reset_stylelist(stylelist)
-                        this.setState({
-                        style: 'viewers',
-                        viewerstyle: stylelist
-                        });
-                        }}
-                    >
-                    通常表示
-                    </button>
+                    </div>
                 </div>
                 <div className={this.state.style}>
                     {this.state.viewports.map((index) => (
                     index<=(this.state.imageIds.length-1)?
                     <div className= {this.state.viewerstyle[index]}
                     onDoubleClick={() => {
-                        stylelist=reset_stylelist(stylelist)
+                        stylelist=reset_list(stylelist,'viewer')
                         stylelist[index]='viewer_big_box';
                         this.setState({
                         style: 'viewers_click',
